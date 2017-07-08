b3399f57a90c39c2658c47f05a4b13e587cf1fce
scarfie <andydaviesnz@gmail.com>
2017-07-08 15:40:34 +0800

    Move directory structure

---
+++ QuoteCalculator.Tests/LoanOfferTests.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit;
+using QuoteCalculator.Models;
+
+namespace QuoteCalculator.Tests {
+	public class LoanOfferTests {
+		[Fact]
+		public void CalculateFutureValue() {
+			var loan = new LoanOffer();
+			loan.Amount = 1000;
+			loan.Rate = 0.07;
+
+			double futureValue = loan.GetTotalRepaymentValue();
+		}
+		
+		[Fact]
+		public void CalculateMonthlyRepayment(){
+			var loan = new LoanOffer();
+			loan.Amount = 1000;
+			loan.Rate = 0.07;
+
+			double monthlyRepayment = loan.GetMonthlyRepaymentValue();
+		}
+	}
+}
