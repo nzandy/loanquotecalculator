fee7c7e5fca58aa880554d624f15d318a1203cda
scarfie <andydaviesnz@gmail.com>
2017-07-11 19:45:07 +0800

    Add more tests.

--- QuoteCalculator.Tests/LoanRequestTests.cs
+++ QuoteCalculator.Tests/LoanRequestTests.cs
@@ -35,7 +35,41 @@ namespace QuoteCalculator.Tests {
 
 		[Fact]
 		public void TotalRepaymentAmount(){
-			
+			var lender = new Lender("Bob", 1000, 0.07);
+			var request = new LoanRequest(1000, 36);
+			request.AddLenderToLoan(lender);
+			var total = request.GetTotalRepaymentAmount();
+			Assert.Equal(1111.58,request.GetTotalRepaymentAmount());
+		}
+
+		[Fact]
+		public void TotalRepaymentAmountTwoLenders(){
+			var lender1 = new Lender("Bob", 500, 0.07);
+			var lender2 = new Lender("Anne", 500, 0.07);
+
+			var request = new LoanRequest(1000, 36);
+			request.AddLenderToLoan(lender1);
+			request.AddLenderToLoan(lender2);
+			Assert.Equal(1111.58,request.GetTotalRepaymentAmount());
+		}
+
+		[Fact] 
+		void MonthlyRepaymentAmount(){
+			var lender = new Lender("Bob", 1000, 0.07);
+			var request = new LoanRequest(1000, 36);
+			request.AddLenderToLoan(lender);
+			Assert.Equal(30.88, request.GetMonthlyRepaymentAmount());
+		}
+
+		[Fact]
+		public void MonthlyRepaymentAmountTwoLenders(){
+			var lender1 = new Lender("Bob", 500, 0.07);
+			var lender2 = new Lender("Anne", 500, 0.07);
+
+			var request = new LoanRequest(1000, 36);
+			request.AddLenderToLoan(lender1);
+			request.AddLenderToLoan(lender2);
+			Assert.Equal(30.88, request.GetMonthlyRepaymentAmount());
 		}
 	}
 }
