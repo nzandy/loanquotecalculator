5a3fc4a250477dac9897f3d29b075614e9e62fc9
scarfie <andydaviesnz@gmail.com>
2017-07-11 22:12:41 +0800

    Add more tests. Include CSV file in output for unit tests.

---
+++ QuoteCalculator.Tests/CsvLenderRepositoryTests.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using Xunit;
+using QuoteCalculator.Repositories;
+using QuoteCalculator.Models;
+
+namespace QuoteCalculator.Tests {
+	public class CsvLenderRepositoryTests {
+		private List<Lender> _lenders;
+		private Lender _firstRecord;
+		public CsvLenderRepositoryTests() {
+			string path = "./TestData/MarketDataForExercise.csv";
+			var repo = new CsvLenderRepository(path);
+			_lenders = repo.GetLenders().ToList();
+			_firstRecord = _lenders.First();
+		}
+
+		[Fact]
+		public void BadRecordNotIncluded(){
+			Assert.False(_lenders.Any(l => l.Name == "MrError"));
+		}
+
+		[Fact]
+		public void ParseLenders(){
+			Assert.Equal(7, _lenders.Count);
+		}
+
+		[Fact]
+		public void ParseLenderName(){
+			Assert.Equal("Bob", _firstRecord.Name);
+		}
+
+		[Fact]
+		public void ParseLenderRate(){
+			Assert.Equal(0.075, _firstRecord.InterestRate);
+		}
+
+		[Fact]
+		public void ParseLenderAvailableAmount(){
+			Assert.Equal(640, _firstRecord.AvailableAmount);
+		}
+	}
+}
